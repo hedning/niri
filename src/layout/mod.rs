b4ee6e5f42c96666ade4d408480a8649a8748faf
Tor Hedin Brønner <torh@solutionseeker.no>
2024-09-27 20:28:57 +0200

    Revert "Animate focus-workspace by idx/back and forth/previous"
    This reverts commit 8eb34b2e185aa0e0affea450226369cd3f9e6a78.
    
    This was just annoying, Don't think workspace animation should trigger
    outside of next/prev workspace (there it makes somewhat sense)

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -1440,7 +1440,7 @@ impl<W: LayoutElement> Layout<W> {
                         Some(WorkspaceSwitch::Gesture(gesture))
                             if gesture.current_idx.floor() == workspace_idx as f64
                                 || gesture.current_idx.ceil() == workspace_idx as f64 => {}
-                        _ => mon.switch_workspace(workspace_idx),
+                        _ => mon.switch_workspace(workspace_idx, true),
                     }
 
                     break;
@@ -2037,7 +2037,7 @@ impl<W: LayoutElement> Layout<W> {
         let Some(monitor) = self.active_monitor() else {
             return;
         };
-        monitor.switch_workspace(idx);
+        monitor.switch_workspace(idx, false);
     }
 
     pub fn switch_workspace_auto_back_and_forth(&mut self, idx: usize) {
@@ -5428,6 +5428,38 @@ mod tests {
         };
 
         assert!(monitors[0].workspaces[1].has_windows());
+    }
+
+    #[test]
+    fn focus_workspace_by_idx_does_not_leave_empty_workspaces() {
+        let ops = [
+            Op::AddOutput(1),
+            Op::AddWindow {
+                id: 0,
+                bbox: Rectangle::from_loc_and_size((0, 0), (100, 200)),
+                min_max_size: Default::default(),
+            },
+            Op::FocusWorkspaceDown,
+            Op::AddWindow {
+                id: 1,
+                bbox: Rectangle::from_loc_and_size((0, 0), (100, 200)),
+                min_max_size: Default::default(),
+            },
+            Op::FocusWorkspaceUp,
+            Op::CloseWindow(0),
+            Op::FocusWorkspace(3),
+        ];
+
+        let mut layout = Layout::default();
+        for op in ops {
+            op.apply(&mut layout);
+        }
+
+        let MonitorSet::Normal { monitors, .. } = layout.monitor_set else {
+            unreachable!()
+        };
+
+        assert!(monitors[0].workspaces[0].has_windows());
     }
 
     #[test]
